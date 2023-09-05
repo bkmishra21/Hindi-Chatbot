f5cac69bc761273fe9497454ad6def5e0f70a6c1
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-09-05 18:32:09 +0530

    UI Design Complete

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -2,62 +2,51 @@ import React, { useState } from 'react'
 import './home.css'
 import Result from './Result';
 export default function Home(props) {
-    const submitvalue=async ()=>{
-       alert("let's see Solution")
-       console.log(props.values);
-       await props.setValues("asjhlffh");
-       console.log(props.values);
-    }
-    const [ user, setUser] = useState({
-      text:"",
-     })
-     const handleChange = e => {
-      const { name, value } = e.target
-      setUser({
-          ...user,
-          [name]: value
-      })
+  const submitvalue = async () => {
+    alert("let's see Solution")
+    console.log(props.values);
+    await props.setValues("asjhlffh");
+    console.log(props.values);
+  }
+  const [user, setUser] = useState({
+    text: "",
+  })
+  const handleChange = e => {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
   }
   const [bot, setbot] = useState(true);
   return (
-    <div>
-    <div className='container my-4'>
-<div class="mb-3">
-    <img src='./medicapslogo.png'  alt='medicaps' className='my-4 alert alert-dark'  style={{ borderRadius:"4px",padding:"10px"}}/>
-    <h2 for="exampleFormControlTextarea1" class="form-label text">WORD SENSE DISAMBIGUATION IN HINDI LANGUAGE USING MACHINE LEARNING</h2>
-  <br/>
-  {/* <div className='row'>
-  <h2 for="exampleFormControlTextarea1" class="form-label col col-lg-3 my-4" style={{paddingTop:"40px"}}>Input Sentence:</h2>
-  <br/>
-  <textarea class="form-control mx-4 col" id="exampleFormControlTextarea1"  type="text" name="text"     value={user.text}            placeholder="Enter Text Here"
-               onChange={ handleChange } rows="7"></textarea>
-  </div>
-  <br/> */}
-  {/* <a className='btn btn-success mx-3' href="/result" >Go To Next Page</a> */}
-</div>
-<br/>
-<div className='row'>
-
-  <div className='col btn btn-secondary mx-2'>
-    <br/>
-    <h2 style={{color:"bisque"}}>Submitted By:</h2>
-    <h2 style={{color:"skyblue"}}>BINOD KUMAR MISHRA</h2>
-    <h2>Ph.D Scholar</h2>
-    <h2>Enrollemnt No. : EN18CS601001</h2>
-    <br/>
-  </div>
-  <div className='col btn btn-secondary mx-2'>
-  <br/>
-  {/* <br/> */}
-  <h2 style={{color:"bisque"}}>Supervisor:</h2>
-    <h2  style={{color:"skyblue"}}>Dr. Suresh Jain</h2>
-    <h2>Professor, Compter Science and Engineering Department</h2>
-    <br/>
-  </div>
-</div>
+    <div className='hundred-vh-container'>
+      <div className="logoTitle-container">
+        <div className="logo-container">
+          <img src='./medicapslogo.png' alt='medicaps' />
+        </div>
+        <div className="title-container">
+          <h3>WORD SENSE DISAMBIGUATION IN HINDI LANGUAGE USING MACHINE LEARNING</h3>
+        </div>
       </div>
-{bot?<Result values={props.value} setValues={props.setValue}/>:null}
-{/* <button className='float-chat-icon' onClick={()=>{setbot(!bot)}}><img src='https://img.icons8.com/?size=512&id=119446&format=png'/></button> */}
+      <div className="content-and-name-container">
+        <div className="names-container">
+          <div className="nameBox-container">
+            <h2 style={{ color: "#941728ff" }}>Submitted By:</h2>
+            <h2 style={{ color: "red" }}>BINOD KUMAR MISHRA</h2>
+            <h2>Ph.D Scholar</h2>
+            <h2>Enrollemnt No. : EN18CS601001</h2>
+          </div>
+          <div className="nameBox-container">
+            <h2 style={{ color: "#941728ff" }}>Supervisor:</h2>
+            <h2 style={{ color: "red" }}>Dr. Suresh Jain</h2>
+            <h2>Professor, Compter Science and Engineering Department</h2>
+          </div>
+        </div>
+        <div className="chatBot-container">
+          {bot ? <Result values={props.value} setValues={props.setValue} /> : null}
+        </div>
       </div>
+    </div >
   )
 }
